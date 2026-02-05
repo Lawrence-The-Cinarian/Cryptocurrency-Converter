2d807d72316c76211f76141de6d1f437a6ac7916
Lawrence The Cinarian <cinarilabs@gmail.com>
2026-02-05 16:53:45 +0100

    Add files via upload

---
+++ ctc.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+
+	int choice; //Choice Of Question
+	double input; //Question
+        double output; //Answer
+        const double BTC_TO_USDT = 75000; /*BTC TO USDT OR USDT TO ETH*/
+        const double ETH_TO_USDT = 2000; /*ETH TO USDT OR USDT TO ETH*/
+        const double CN_TO_CUSD = 40000; /*CN TO CUSD OR CUSD TO CN*/
+do {
+   printf("Welcome to Cinel's Crypto Converter\n");
+   printf("Enter Number for Conversion: ");
+   scanf("%lf", &input);
+    if(input <= 0) {
+   printf("Invalid Input\n");
+  }
+   printf("\n");
+   printf("(1) BTC TO USDT\n");
+   printf("(2) USDT TO BTC\n");
+   printf("(3) ETH TO USDT\n");
+   printf("(4) USDT TO ETH\n");
+   printf("(5) CN TO CUSD\n");
+   printf("(6) CUSD TO CN\n");
+   printf("\n");
+   printf("Enter Number for Currency: ");
+   scanf("%d", &choice);
+    if(choice <= 0 || choice >= 7) {
+   printf("Invalid Input\n");
+  return choice;
+  }
+   switch(choice) {
+	case 1:
+ 	   output = input * BTC_TO_USDT;
+	break;
+	case 2:
+           output = input / BTC_TO_USDT;
+	break;
+	case 3:
+           output = input * ETH_TO_USDT;
+        break;
+	case 4:
+           output = input / ETH_TO_USDT;
+        break;
+	case 5:
+           output = input * CN_TO_CUSD;
+        break;
+	case 6:
+           output = input / CN_TO_CUSD;
+        break;
+	default:
+	printf("Invalid Input\n");
+   }
+   printf("Amount: %.9f\n", output);
+   printf("\n");
+   printf("****************************************************************\n");
+   printf("\n");
+}
+while(choice != 0);
+return 0;
+}
