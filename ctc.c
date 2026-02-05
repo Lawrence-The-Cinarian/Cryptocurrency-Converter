07a9a64518ffbbd91cd04076714672fb74b2b2fe
Lawrence The Cinarian <cinarilabs@gmail.com>
2026-02-05 13:05:15 +0100

    Create ctc.c

---
+++ ctc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+/*CUSD = USDT = USD, All are One Peg
+CUSD = $1*/
+int choice
+double num;
+const double ETH/USDT = 2000; /*ETH TO USDT or USDT TO ETH*/
+const double BTC/USDT = 75000; /*BTC TO USDT or USDT TO BTC*)
+const double CN/CUSD = 1000; /*CN TO CUSD or CUSD TO CN*/
+
+printf("Cinel Network");
+printf("What conversion are you doing today: ");
+}
